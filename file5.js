de0738b1b40df25e5273e83b512ae2395a176c26
Son Vu javascript <hoaison1711@gmail.com>
2024-04-10 15:20:13 -0400

    Hello

--- file5.js
+++ file5.js
@@ -1,5 +1,4 @@
 // cho 1 array, output ra 1 cai array khac, array do la binh phuong cac phan tu cua array hien tai
 let array = [ 1, 2, 3, 4, 5];
-array.push();
-console.log(array);
-array = Math.pow(array, 2)
+array.unshift(12,12,12);
+console.log(array)
