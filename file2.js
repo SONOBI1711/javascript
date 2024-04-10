d1373cbdaf4540e03ce0f07ea8423990e61a44ff
Son Vu javascript <hoaison1711@gmail.com>
2024-04-10 08:10:34 -0400

    Change content of file1.js

--- file2.js
+++ file2.js
@@ -1,9 +1,9 @@
 // cho 1 array co 10 so bat ky, tim so lon nhat trong array
 let array = [4, 2, 7, 1, 10, 6, 3, 8, 20, 2];
-let max = array[0]
+let maxNumber = array[0]
 array.forEach((element) => {
-   if (element > max) {
-    max = element;
+   if (element > maxNumber) {
+    maxNumber = element;
    }  
 })
-console.log(max)
+console.log(maxNumber);
